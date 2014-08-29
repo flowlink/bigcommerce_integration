f26f92364f763e4385c134691f0a2c5e8fa837cd
Adam Harvie <adam.harvie@gmail.com>
2014-08-29 14:05:14 -0400

    Added Wombat authentication config

--- app/config_dev.php
+++ app/config_dev.php
@@ -7,5 +7,9 @@ $app['callback_url']		= "http://walrusk.net/oauth";
 
 $app['bc_api_base'] 		= "https://api.bigcommerce.com";
 
+//Wombat authentication tokens
+$app['wombat_store']		= "53cffa32776f6d6e34740000";
+$app['wombat_token']		= "db4ebe193caad7c75589bc9a865a4d6925dad15f50e4ca69";
+
 //For the basic file-based user persistence, set a local directory to save in
 $app['user.persist_path'] = '/tmp/sprout';
