2c13986e9b0718ea9e27fb1425dae99dc0ed81c6
Adam Harvie <adam.harvie@gmail.com>
2014-09-04 15:40:58 -0400

    Added add_customer action

--- src/Sprout/Wombat/Controller/WombatController.php
+++ src/Sprout/Wombat/Controller/WombatController.php
@@ -250,6 +250,41 @@ class WombatController {
 		}
 	}
 
+	public function postCustomerAction(Request $request, Application $app) {
+		$request_data = $this->initRequestData($request);
+		
+		$client = $this->legacyAPIClient($request_data['legacy_api_info']);
+
+		$wombat_data = $request->request->get('customer');
+		
+		$bcModel = new Customer($wombat_data,'wombat');
+		$bc_data = $bcModel->getBigCommerceObject('create');
+
+		
+		$options = array(
+			'headers'=>array('Content-Type'=>'application/json'),
+			'body' => (string)json_encode($bc_data),
+			//'debug'=>fopen('debug.txt', 'w')
+			);
+
+		//return $options['body'].PHP_EOL;
+		
+		$response = $client->post('customers',$options);
+		// @todo: the Guzzle client will intervene with its own error response before we get to our error below,
+		// make it not do that or catch an exception rather than checking code
+
+		if($response->getStatusCode() != 201) {
+			throw new Exception($request_data['request_id'].":Error received from BigCommerce ".$response->getBody(),500);
+		} else {
+			//return our success code & data
+			$response = array(
+				'request_id' => $request_data['request_id'],
+				'summary' => "The customer $bc_data->first_name $bc_data->last_name was created in BigCommerce",
+				);
+			return $app->json($response,200);
+		}
+	}
+
 	/*
 	
 		SHIPMENT ACTIONS
@@ -447,8 +482,6 @@ class WombatController {
 
 	//add
 	public function postShipmentAction(Request $request, Application $app) {
-	}
-	public function postCustomerAction(Request $request, Application $app) {
 	}
 
 	//update
