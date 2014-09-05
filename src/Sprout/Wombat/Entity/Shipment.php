435c6f6e1204d4be7a6163ca1ef2ea4e0727d53d
Adam Harvie <adam.harvie@gmail.com>
2014-09-05 16:56:31 -0400

    Added prepareBCResources to Shipment entity to get required data from BC for adding/updating a shipment

--- src/Sprout/Wombat/Entity/Shipment.php
+++ src/Sprout/Wombat/Entity/Shipment.php
@@ -57,12 +57,92 @@ class Shipment {
 	 * Get a BigCommerce-formatted set of data from a Wombat one.
 	 */
 	public function getBigCommerceObject($action = 'create') {
-		if(!$this->data) {
+		if(isset($this->data['bc']))
+			return $this->data['bc'];
+		else if(isset($this->data['wombat']))
+			$wombat_obj = (object) $this->data['wombat'];
+		else
 			return false;
+
+		/*
+			BC required:
+
+			order_address_id
+			items
+		*/
+
+		$bc_obj = (object) array(
+			'order_address_id' => $wombat_obj->_order_address_id,
+			'tracking_number' => $wombat_obj->tracking,
+			'comments' => '',
+			);
+
+		foreach($wombat_obj->items as $item) {
+			$bc_obj->items[] = (object) array(
+				'order_product_id' => $item['_order_product_id'],
+				'quantity' => $item['quantity'],
+			);
 		}
 
-		$bc = new stdClass();
+		$this->data['bc'] = $bc_obj;
+		return $bc_obj;
+	}
 
-		return $bc;
+	public function prepareBCResources($client) {
+
+		//retrieve shipping addresses already associated with the order_id
+		$wombat_obj = (object)$this->data['wombat'];
+		$order_id = $wombat_obj->order_id;
+		
+		$response = $client->get("orders/$order_id/shipping_addresses");
+					
+		if(intval($response->getStatusCode()) === 200)
+			//$this->data['bc']->$resource_name = $response->json(array('object'=>TRUE));
+			$addresses = $response->json(array('object'=>TRUE));
+		else
+			//$this->data['bc']->$resource_name = NULL;
+			$addresses = NULL;
+
+		//check each address against the one we've been passed, use the ID from the first one that matches
+		if(is_array($addresses)) {
+			foreach($addresses as $address) {
+				if(
+					$address->first_name 		== $wombat_obj->shipping_address['firstname']	&&
+					$address->last_name 		== $wombat_obj->shipping_address['lastname']	&&
+					$address->street_1 			== $wombat_obj->shipping_address['address1']	&&
+					$address->street_2 			== $wombat_obj->shipping_address['address2']	&&
+					$address->city 					== $wombat_obj->shipping_address['city']			&&
+					$address->state 				== $wombat_obj->shipping_address['state']			&&
+					$address->country_iso2 	== $wombat_obj->shipping_address['country']		&&
+					$address->zip 					== $wombat_obj->shipping_address['zipcode']
+					) {
+					$this->data['wombat']['_order_address_id'] = $address->id;
+				}
+				
+			}
+		}
+
+		// get order products for the order_product_id
+		$response = $client->get("orders/$order_id/products");
+
+		if(intval($response->getStatusCode()) === 200)
+			//$this->data['bc']->$resource_name = $response->json(array('object'=>TRUE));
+			$products = $response->json(array('object'=>TRUE));
+		else
+			//$this->data['bc']->$resource_name = NULL;
+			$products = NULL;
+
+		// go through the resulting products and match the product_ids to get the order_product_id
+		if(is_array($products)) {
+			foreach($products as $product) {
+				
+				foreach($this->data['wombat']['items'] as $index => $item) {
+					if($product->product_id == $item['product_id']) {
+						$this->data['wombat']['items'][$index]['_order_product_id'] = $product->id;
+					}
+				}
+
+			}
+		}
 	}
 }
