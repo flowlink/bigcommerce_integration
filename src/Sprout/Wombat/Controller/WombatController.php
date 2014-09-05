27d4f99096b31bd66617248cdff52a11120eecd1
Adam Harvie <adam.harvie@gmail.com>
2014-09-05 11:43:54 -0400

    Added setInventory action (in progress)

--- src/Sprout/Wombat/Controller/WombatController.php
+++ src/Sprout/Wombat/Controller/WombatController.php
@@ -150,6 +150,38 @@ class WombatController {
 		}
 	}
 
+	public function setInventoryAction(Request $request, Application $app) {
+		$request_data = $this->initRequestData($request);
+
+		$client = $this->legacyAPIClient($request_data['legacy_api_info']);
+
+		$wombat_data = $request->request->get('inventory');
+
+		$bcModel = new Product($wombat_data,'wombat');
+		$bc_data = $bcModel->getBigCommerceObject('set_inventory');
+
+		$options = array(
+			'headers'=>array('Content-Type'=>'application/json'),
+			'body' => (string)json_encode($bc_data),
+			//'debug'=>fopen('debug.txt', 'w')
+			);
+
+		$response = $client->put('products/'.$wombat_data['product_id'],$options);
+		// @todo: the Guzzle client will intervene with its own error response before we get to our error below,
+		// make it not do that or catch an exception rather than checking code
+
+		if($response->getStatusCode() != 200) {
+			throw new Exception($request_data['request_id'].":Error received from BigCommerce ".$response->getBody(),500);
+		} else {
+			//return our success code & data
+			$response = array(
+				'request_id' => $request_data['request_id'],
+				'summary' => "The product $bc_data->name was updated in BigCommerce",
+				);
+			return $app->json($response,200);
+		}
+	}
+
 	/*
 	
 		ORDER ACTIONS
@@ -692,8 +724,6 @@ class WombatController {
 	}
 
 	//set
-	public function setInventoryAction(Request $request, Application $app) {
-	}
 
 	/**
 	 * Construct a BC request URL
