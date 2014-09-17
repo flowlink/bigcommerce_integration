8a8a15f7bdb9ae097da8c297d9f1c3b464c74105
Adam Harvie <adam.harvie@gmail.com>
2014-09-17 08:45:49 -0400

    Customer updates

--- src/Sprout/Wombat/Entity/Customer.php
+++ src/Sprout/Wombat/Entity/Customer.php
@@ -52,7 +52,7 @@ class Customer {
 				'zipcode' 	=> $address->zip,
 				'city' 			=> $address->city,
 				'state' 		=> $address->state,
-				'country' 	=> $address->country_code,
+				'country' 	=> $address->country_iso2,
 				'phone' 		=> $address->phone,
 				'BCID' 			=> $address->id,
 			);
@@ -68,7 +68,7 @@ class Customer {
 				'zipcode' 	=> $address->zip,
 				'city' 			=> $address->city,
 				'state' 		=> $address->state,
-				'country' 	=> $address->country_code,
+				'country' 	=> $address->country_iso2,
 				'phone' 		=> $address->phone,
 				'BCID' 			=> $address->id,
 			);
@@ -163,5 +163,335 @@ class Customer {
 			}
 			
 		}
+	}
+
+	/**
+	 * Send data to BigCommerce that's handled separately from the main customer object:
+	 * addresses
+	 */
+	public function pushAttachedResources($client,$request_data) {
+
+		$wombat_obj = (object) $this->data['wombat'];
+
+		//get the customer ID via their email
+		$id = $this->getBCID($client,$request_data);
+		$path = "customers/$id/addresses";
+		$options = array(
+			'headers'=>array('Content-Type'=>'application/json'),
+			);
+
+		if(!empty($wombat_obj->billing_address)) {
+			$billing_address = (object) array(
+				
+			  "first_name"	=> $wombat_obj->firstname,
+			  "last_name"		=> $wombat_obj->lastname,
+			  "company"			=> '',
+			  "street_1"		=> $wombat_obj->billing_address['address1'],
+			  "street_2"		=> $wombat_obj->billing_address['address2'],
+			  "city"				=> $wombat_obj->billing_address['city'],
+			  "state"				=> $wombat_obj->billing_address['state'],
+			  "zip"					=> $wombat_obj->billing_address['zipcode'],
+			  "country"			=> $this->getCountryName($wombat_obj->billing_address['country']),
+			  "phone"				=> $wombat_obj->billing_address['phone'],
+
+				);
+
+			$options['body'] = (string)json_encode($billing_address);
+			//echo print_r($options['body'],true).PHP_EOL;
+			try {
+				$client->post($path,$options);
+			}
+			catch(\Exception $e) {
+				throw new \Exception($request_data['request_id'].":::::Error received from BigCommerce while adding customer address: ".$e->getMessage(),500);
+			}
+		}
+
+
+		if(!empty($wombat_obj->shipping_address)) {
+			$shipping_address = (object) array(
+				
+			  "first_name"	=> $wombat_obj->firstname,
+			  "last_name"		=> $wombat_obj->lastname,
+			  "company"			=> '',
+			  "street_1"		=> $wombat_obj->shipping_address['address1'],
+			  "street_2"		=> $wombat_obj->shipping_address['address2'],
+			  "city"				=> $wombat_obj->shipping_address['city'],
+			  "state"				=> $wombat_obj->shipping_address['state'],
+			  "zip"					=> $wombat_obj->shipping_address['zipcode'],
+			  "country"			=> $this->getCountryName($wombat_obj->shipping_address['country']),
+			  "phone"				=> $wombat_obj->shipping_address['phone'],
+
+				);
+
+			$options['body'] = (string)json_encode($shipping_address);
+			//echo print_r($options['body'],true).PHP_EOL;
+			try {
+				$client->post($path,$options);
+			}
+			catch(\Exception $e) {
+				throw new \Exception($request_data['request_id'].":::::Error received from BigCommerce while adding customer address: ".$e->getMessage(),500);
+			}
+		}
+		
+	}
+
+	/**
+	 * Convert a country iso code to a full name
+	 */
+	private function getCountryName($iso) {
+		$countries = array(
+			'AF' => 'Afghanistan',
+			'AX' => 'Aland Islands',
+			'AL' => 'Albania',
+			'DZ' => 'Algeria',
+			'AS' => 'American Samoa',
+			'AD' => 'Andorra',
+			'AO' => 'Angola',
+			'AI' => 'Anguilla',
+			'AQ' => 'Antarctica',
+			'AG' => 'Antigua And Barbuda',
+			'AR' => 'Argentina',
+			'AM' => 'Armenia',
+			'AW' => 'Aruba',
+			'AU' => 'Australia',
+			'AT' => 'Austria',
+			'AZ' => 'Azerbaijan',
+			'BS' => 'Bahamas',
+			'BH' => 'Bahrain',
+			'BD' => 'Bangladesh',
+			'BB' => 'Barbados',
+			'BY' => 'Belarus',
+			'BE' => 'Belgium',
+			'BZ' => 'Belize',
+			'BJ' => 'Benin',
+			'BM' => 'Bermuda',
+			'BT' => 'Bhutan',
+			'BO' => 'Bolivia',
+			'BA' => 'Bosnia And Herzegovina',
+			'BW' => 'Botswana',
+			'BV' => 'Bouvet Island',
+			'BR' => 'Brazil',
+			'IO' => 'British Indian Ocean Territory',
+			'BN' => 'Brunei Darussalam',
+			'BG' => 'Bulgaria',
+			'BF' => 'Burkina Faso',
+			'BI' => 'Burundi',
+			'KH' => 'Cambodia',
+			'CM' => 'Cameroon',
+			'CA' => 'Canada',
+			'CV' => 'Cape Verde',
+			'KY' => 'Cayman Islands',
+			'CF' => 'Central African Republic',
+			'TD' => 'Chad',
+			'CL' => 'Chile',
+			'CN' => 'China',
+			'CX' => 'Christmas Island',
+			'CC' => 'Cocos (Keeling) Islands',
+			'CO' => 'Colombia',
+			'KM' => 'Comoros',
+			'CG' => 'Congo',
+			'CD' => 'Congo, Democratic Republic',
+			'CK' => 'Cook Islands',
+			'CR' => 'Costa Rica',
+			'CI' => 'Cote D\'Ivoire',
+			'HR' => 'Croatia',
+			'CU' => 'Cuba',
+			'CY' => 'Cyprus',
+			'CZ' => 'Czech Republic',
+			'DK' => 'Denmark',
+			'DJ' => 'Djibouti',
+			'DM' => 'Dominica',
+			'DO' => 'Dominican Republic',
+			'EC' => 'Ecuador',
+			'EG' => 'Egypt',
+			'SV' => 'El Salvador',
+			'GQ' => 'Equatorial Guinea',
+			'ER' => 'Eritrea',
+			'EE' => 'Estonia',
+			'ET' => 'Ethiopia',
+			'FK' => 'Falkland Islands (Malvinas)',
+			'FO' => 'Faroe Islands',
+			'FJ' => 'Fiji',
+			'FI' => 'Finland',
+			'FR' => 'France',
+			'GF' => 'French Guiana',
+			'PF' => 'French Polynesia',
+			'TF' => 'French Southern Territories',
+			'GA' => 'Gabon',
+			'GM' => 'Gambia',
+			'GE' => 'Georgia',
+			'DE' => 'Germany',
+			'GH' => 'Ghana',
+			'GI' => 'Gibraltar',
+			'GR' => 'Greece',
+			'GL' => 'Greenland',
+			'GD' => 'Grenada',
+			'GP' => 'Guadeloupe',
+			'GU' => 'Guam',
+			'GT' => 'Guatemala',
+			'GG' => 'Guernsey',
+			'GN' => 'Guinea',
+			'GW' => 'Guinea-Bissau',
+			'GY' => 'Guyana',
+			'HT' => 'Haiti',
+			'HM' => 'Heard Island & Mcdonald Islands',
+			'VA' => 'Holy See (Vatican City State)',
+			'HN' => 'Honduras',
+			'HK' => 'Hong Kong',
+			'HU' => 'Hungary',
+			'IS' => 'Iceland',
+			'IN' => 'India',
+			'ID' => 'Indonesia',
+			'IR' => 'Iran, Islamic Republic Of',
+			'IQ' => 'Iraq',
+			'IE' => 'Ireland',
+			'IM' => 'Isle Of Man',
+			'IL' => 'Israel',
+			'IT' => 'Italy',
+			'JM' => 'Jamaica',
+			'JP' => 'Japan',
+			'JE' => 'Jersey',
+			'JO' => 'Jordan',
+			'KZ' => 'Kazakhstan',
+			'KE' => 'Kenya',
+			'KI' => 'Kiribati',
+			'KR' => 'Korea',
+			'KW' => 'Kuwait',
+			'KG' => 'Kyrgyzstan',
+			'LA' => 'Lao People\'s Democratic Republic',
+			'LV' => 'Latvia',
+			'LB' => 'Lebanon',
+			'LS' => 'Lesotho',
+			'LR' => 'Liberia',
+			'LY' => 'Libyan Arab Jamahiriya',
+			'LI' => 'Liechtenstein',
+			'LT' => 'Lithuania',
+			'LU' => 'Luxembourg',
+			'MO' => 'Macao',
+			'MK' => 'Macedonia',
+			'MG' => 'Madagascar',
+			'MW' => 'Malawi',
+			'MY' => 'Malaysia',
+			'MV' => 'Maldives',
+			'ML' => 'Mali',
+			'MT' => 'Malta',
+			'MH' => 'Marshall Islands',
+			'MQ' => 'Martinique',
+			'MR' => 'Mauritania',
+			'MU' => 'Mauritius',
+			'YT' => 'Mayotte',
+			'MX' => 'Mexico',
+			'FM' => 'Micronesia, Federated States Of',
+			'MD' => 'Moldova',
+			'MC' => 'Monaco',
+			'MN' => 'Mongolia',
+			'ME' => 'Montenegro',
+			'MS' => 'Montserrat',
+			'MA' => 'Morocco',
+			'MZ' => 'Mozambique',
+			'MM' => 'Myanmar',
+			'NA' => 'Namibia',
+			'NR' => 'Nauru',
+			'NP' => 'Nepal',
+			'NL' => 'Netherlands',
+			'AN' => 'Netherlands Antilles',
+			'NC' => 'New Caledonia',
+			'NZ' => 'New Zealand',
+			'NI' => 'Nicaragua',
+			'NE' => 'Niger',
+			'NG' => 'Nigeria',
+			'NU' => 'Niue',
+			'NF' => 'Norfolk Island',
+			'MP' => 'Northern Mariana Islands',
+			'NO' => 'Norway',
+			'OM' => 'Oman',
+			'PK' => 'Pakistan',
+			'PW' => 'Palau',
+			'PS' => 'Palestinian Territory, Occupied',
+			'PA' => 'Panama',
+			'PG' => 'Papua New Guinea',
+			'PY' => 'Paraguay',
+			'PE' => 'Peru',
+			'PH' => 'Philippines',
+			'PN' => 'Pitcairn',
+			'PL' => 'Poland',
+			'PT' => 'Portugal',
+			'PR' => 'Puerto Rico',
+			'QA' => 'Qatar',
+			'RE' => 'Reunion',
+			'RO' => 'Romania',
+			'RU' => 'Russian Federation',
+			'RW' => 'Rwanda',
+			'BL' => 'Saint Barthelemy',
+			'SH' => 'Saint Helena',
+			'KN' => 'Saint Kitts And Nevis',
+			'LC' => 'Saint Lucia',
+			'MF' => 'Saint Martin',
+			'PM' => 'Saint Pierre And Miquelon',
+			'VC' => 'Saint Vincent And Grenadines',
+			'WS' => 'Samoa',
+			'SM' => 'San Marino',
+			'ST' => 'Sao Tome And Principe',
+			'SA' => 'Saudi Arabia',
+			'SN' => 'Senegal',
+			'RS' => 'Serbia',
+			'SC' => 'Seychelles',
+			'SL' => 'Sierra Leone',
+			'SG' => 'Singapore',
+			'SK' => 'Slovakia',
+			'SI' => 'Slovenia',
+			'SB' => 'Solomon Islands',
+			'SO' => 'Somalia',
+			'ZA' => 'South Africa',
+			'GS' => 'South Georgia And Sandwich Isl.',
+			'ES' => 'Spain',
+			'LK' => 'Sri Lanka',
+			'SD' => 'Sudan',
+			'SR' => 'Suriname',
+			'SJ' => 'Svalbard And Jan Mayen',
+			'SZ' => 'Swaziland',
+			'SE' => 'Sweden',
+			'CH' => 'Switzerland',
+			'SY' => 'Syrian Arab Republic',
+			'TW' => 'Taiwan',
+			'TJ' => 'Tajikistan',
+			'TZ' => 'Tanzania',
+			'TH' => 'Thailand',
+			'TL' => 'Timor-Leste',
+			'TG' => 'Togo',
+			'TK' => 'Tokelau',
+			'TO' => 'Tonga',
+			'TT' => 'Trinidad And Tobago',
+			'TN' => 'Tunisia',
+			'TR' => 'Turkey',
+			'TM' => 'Turkmenistan',
+			'TC' => 'Turks And Caicos Islands',
+			'TV' => 'Tuvalu',
+			'UG' => 'Uganda',
+			'UA' => 'Ukraine',
+			'AE' => 'United Arab Emirates',
+			'GB' => 'United Kingdom',
+			'US' => 'United States',
+			'UM' => 'United States Outlying Islands',
+			'UY' => 'Uruguay',
+			'UZ' => 'Uzbekistan',
+			'VU' => 'Vanuatu',
+			'VE' => 'Venezuela',
+			'VN' => 'Viet Nam',
+			'VG' => 'Virgin Islands, British',
+			'VI' => 'Virgin Islands, U.S.',
+			'WF' => 'Wallis And Futuna',
+			'EH' => 'Western Sahara',
+			'YE' => 'Yemen',
+			'ZM' => 'Zambia',
+			'ZW' => 'Zimbabwe',
+		);
+
+		$iso = strtoupper($iso);
+		$output = '';
+		if(array_key_exists($iso, $countries)) {
+			$output = $countries[$iso];
+		}
+		return $output;
 	}
 }
